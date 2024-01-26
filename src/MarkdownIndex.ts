ab72e89334525c214ee9b4e9e758e750efc1faa5
kodyqywang <kodyqywang@tencent.com>
2024-01-26 17:29:12 +0800

    style: code style format

--- src/MarkdownIndex.ts
+++ src/MarkdownIndex.ts
@@ -2,104 +2,114 @@ import { workspace } from "vscode";
 
 /**
  * The main class to add markdown index.
- * 
+ *
  * Inspired by firjq (https://github.com/firejq/markdown_index/blob/master/src/MarkdownIndex.ts)
- * 
+ *
  * modify by legendmohe
  */
 export class MarkdownIndex {
+    // index base configuration for user, default value is "#"
+    private _indexBase: string = "#";
 
-  // index base configuration for user, default value is "#"
-  private _indexBase: string = "#";
+    constructor() {
+        // load the configuration
+        const configuration = workspace.getConfiguration("markdownIndex");
+        const configBase = configuration.get<string>("indexBase");
+        if (configBase && configBase.length > 0) {
+            this._indexBase = configBase;
+        }
+    }
 
-  constructor() {
-      // load the configuration
-      const configuration = workspace.getConfiguration("markdownIndex"); 
-      const configBase = configuration.get<string>("indexBase");
-      if (configBase && configBase.length > 0) {
-          this._indexBase = configBase;
-      }
-  }
+    private _addPrefix(line: string, prefix: string, markCount: number) {
+        // remove previous index
+        let markIndex = line.indexOf(this._indexBase);
+        if (markIndex == -1) {
+            markIndex = 0;
+        }
+        var re = new RegExp(
+            "(^\\s*\\" + this._indexBase + "+)\\s*((\\d+\\.)+)\\s+",
+            "g"
+        );
+        line = line.replace(re, "$1");
+        return (
+            line.substr(0, markIndex + markCount) +
+            " " +
+            prefix +
+            " " +
+            line.substr(markIndex + markCount).trim()
+        );
+    }
 
-  private _addPrefix(line: string, prefix: string, markCount: number) {
-      // remove previous index
-      let markIndex = line.indexOf(this._indexBase);
-      if (markIndex == -1) {
-          markIndex = 0;
-      }
-      var re = new RegExp('(^\\s*\\' + this._indexBase + '+)\\s*((\\d+\\.)+)\\s+', "g");
-      line = line.replace(re, "$1");
-      return line.substr(0, markIndex + markCount)
-          + " "
-          + prefix
-          + " "
-          + line.substr(markIndex + markCount).trim();
-  }
+    private _countStartsWith(fliter, chars: string[]): number {
+        let count: number = 0;
+        chars.some((element) => {
+            if (fliter(element)) {
+                count++;
+                return false;
+            } else {
+                return true;
+            }
+        });
+        return count;
+    }
 
-  private _countStartsWith(fliter, chars: string[]): number {
-      let count: number = 0;
-      chars.some(element => {
-          if (fliter(element)) {
-              count++;
-              return false;
-          } else {
-              return true;
-          }
-      });
-      return count;
-  }
+    private _addIndex(
+        content: string[],
+        lastMarkCount: number,
+        prefix: string,
+        cursor: number
+    ): number {
+        // leave the normal line and count this._indexBase
+        let targetMarkCount = 0;
+        // mark the current line whether is in the ``` code area
+        let isInCodeArea = false;
 
-  private _addIndex(content: string[], lastMarkCount: number, prefix: string, cursor: number): number {
-      // leave the normal line and count this._indexBase
-      let targetMarkCount = 0;
-      // mark the current line whether is in the ``` code area
-      let isInCodeArea = false;
+        while (cursor < content.length) {
+            let line = content[cursor];
 
-      while (cursor < content.length) {
-          let line = content[cursor];
+            if (line.startsWith("```")) {
+                isInCodeArea = !isInCodeArea;
+                cursor++;
+                continue;
+            }
 
-          if (line.startsWith("```")) {
-            isInCodeArea = !isInCodeArea;
+            if (isInCodeArea == false && line.startsWith(this._indexBase)) {
+                // find the start mark count
+                targetMarkCount = this._countStartsWith((x) => {
+                    return x == this._indexBase;
+                }, line.split(""));
+                break;
+            }
             cursor++;
-            continue;
         }
 
-          if (isInCodeArea == false && line.startsWith(this._indexBase)) {
-              // find the start mark count
-              targetMarkCount = this._countStartsWith(
-                  x => { return x == this._indexBase },
-                  line.split("")
-              );
-              break;
-          }
-          cursor++;
-          
-      }
-  
-      let seq = 1;
-      while (cursor < content.length) {
-          let markCount = this._countStartsWith(
-              x => { return x == this._indexBase },
-              content[cursor].split("")
-          );
-          if (markCount == targetMarkCount && markCount > lastMarkCount) {
-              let curPrefix = prefix + seq + ".";
-              content[cursor] = this._addPrefix(content[cursor], curPrefix, markCount);
-              seq++;
-              // deep first search
-              cursor = this._addIndex(content, markCount, curPrefix, cursor + 1);
-          } else if (markCount <= lastMarkCount) {
-              // rollback 1 row
-              cursor--;
-              break;
-          }
-          cursor++;
-      }
-      return cursor;
-  }
+        let seq = 1;
+        while (cursor < content.length) {
+            let markCount = this._countStartsWith((x) => {
+                return x == this._indexBase;
+            }, content[cursor].split(""));
+            if (markCount == targetMarkCount && markCount > lastMarkCount) {
+                let curPrefix = prefix + seq + ".";
+                content[cursor] = this._addPrefix(
+                    content[cursor],
+                    curPrefix,
+                    markCount
+                );
+                seq++;
+                // deep first search
+                cursor = this._addIndex(content, markCount, curPrefix, cursor + 1);
+            } else if (markCount <= lastMarkCount) {
+                // rollback 1 row
+                cursor--;
+                break;
+            }
+            cursor++;
+        }
+        return cursor;
+    }
 
-  public addMarkdownIndex(content: string[]) {
-      this._addIndex(content, 0, "", 0);
-      return content;
-  }
+    public addMarkdownIndex(content: string[]) {
+        this._addIndex(content, 0, "", 0);
+        return content;
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { MarkdownIndex } from './MarkdownIndex';
+import * as vscode from "vscode";
+import { MarkdownIndex } from "./MarkdownIndex";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     // console.log('Congratulations, your extension "legendmohe" is now active!');
@@ -15,39 +14,44 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.addMarkdownIndex', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand(
+        "extension.addMarkdownIndex",
+        () => {
+            // The code you place here will be executed every time your command is executed
 
-        var editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            vscode.window.showInformationMessage('No open text editor');
-            return; // No open text editor
+            var editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                vscode.window.showInformationMessage("No open text editor");
+                return; // No open text editor
+            }
+
+            var selection = editor.selection;
+            var text = editor.document.getText(selection);
+            var lines: string[];
+            if (text.length == 0) {
+                // use all text if no selection
+                lines = editor.document.getText().split("\n");
+                selection = new vscode.Selection(0, 0, lines.length, 0);
+            } else {
+                lines = text.split("\n");
+            }
+
+            // apply plugin
+            const markdownIndex = new MarkdownIndex();
+            markdownIndex.addMarkdownIndex(lines);
+
+            editor.edit(function (builder: vscode.TextEditorEdit) {
+                var resultText = lines.join("\n");
+                builder.replace(
+                    new vscode.Range(selection.start, selection.end),
+                    resultText
+                );
+            });
         }
-
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
-        var lines: string[];
-        if (text.length == 0) {
-            // use all text if no selection
-            lines = editor.document.getText().split("\n");
-            selection = new vscode.Selection(0, 0, lines.length, 0);
-        } else {
-            lines = text.split("\n");
-        }
-
-        // apply plugin
-        const markdownIndex = new MarkdownIndex();
-        markdownIndex.addMarkdownIndex(lines);
-
-        editor.edit(function (builder: vscode.TextEditorEdit) {
-            var resultText = lines.join("\n");
-            builder.replace(new vscode.Range(selection.start, selection.end), resultText);
-        })
-    });
+    );
 
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() { }
